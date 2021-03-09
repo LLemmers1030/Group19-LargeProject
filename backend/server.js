18317099b33aaf6110a6b19be2324bd54930cbcf
Andrew13560 <andrew13560@gmail.com>
2021-03-09 10:50:14 -0500

    test 2 for server.js

--- backend/server.js
+++ backend/server.js
@@ -65,7 +65,7 @@ app.use(express.static(path.join(__dirname, "frontend/build")));
 // });
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build/index.html"));
+  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
 });
 
 const PORT = process.env.PORT || 8080;
