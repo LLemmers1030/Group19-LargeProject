13e57f8fd5c2cdbe396961e1fc8d389d6bafdee5
Lyndon-John Lemmers <llemmers1030@yahoo.com>
2021-04-03 15:42:43 -0400

    added input form validation

--- frontend/src/pages/signin.js
+++ frontend/src/pages/signin.js
@@ -10,6 +10,94 @@ import image2 from '../images/svg-7.svg'
 
 class SignInPage extends Component {
 
+  constructor(){
+    super();
+    
+    this.state = {
+      input: {},
+      errors: {}
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    let input = this.state.input;
+    input[event.target.name] = event.target.value;
+
+    this.setState({
+      input
+    });
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+
+    if (this.validate()){
+      console.log(this.state);
+
+      let input ={};
+      input["email"] = "";
+      input["password"] = "";
+      input["confirm_password"] = "";
+      this.setState({input:input});
+
+      
+      alert('Form submitted');
+    }
+  }
+
+  validate(){
+    let input = this.state.input;
+    let errors = {};
+    let isValid = true;
+
+    if (!input["email"]){
+      isValid = false;
+      errors["email"] = "Please enter email";
+      //errors["email"] = alert("Please enter email");
+    }
+    
+    if (typeof input["email"] !== "undefined") {
+      
+      var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+
+      if (!pattern.test(input["email"])) {
+        isValid = false;
+        errors["email"] = "Please enter valid email address.";
+        //errors["email"] = alert("Please enter valid email address.");
+      }
+    }
+
+    if (!input["password"]){
+      isValid = false;
+      errors["password"] = "Please enter your password";
+      //errors["password"] = alert("Please enter your password");
+    }
+
+    if (!input["confirm_password"]){
+      isValid = false;
+      errors["confirm_password"] = "Please enter your confirm password";
+      //errors["confirm_password"] = alert("Please enter your confirm password");
+    }
+
+    if (typeof input["password"] != "undefined" && typeof input["confirm_password"] !== "undefined")
+    {
+        if (input["password"] != input["confirm_password"]){
+          isValid = false;
+          errors["password"] = "Passwords don't match";
+          //errors["password"] = alert("Passwords don't match");
+        }
+    }
+
+    this.setState({
+      errors : errors
+    });
+
+    return isValid;
+  }
+
   componentDidMount() {
     const sign_in_btn = document.querySelector("#sign-in-btn");
     const sign_up_btn = document.querySelector("#sign-up-btn");
@@ -36,16 +124,18 @@ class SignInPage extends Component {
         <div className="container">
           <div className="forms-container">
             <div className="signin-signup">
-              <form className="sign-in-form">
+              <form className="sign-in-form" onSubmit={this.handleSubmit}>
                 <h2 className="title">Sign in</h2>
                 <div className="input-field">
                   <i className="fas fa-user" />
-                  <input type="text" placeholder="Username" />
+                  <input type="text" placeholder="Email" />
                 </div>
+                <p className="text-danger">{this.state.errors.email}</p>
                 <div className="input-field">
                   <i className="fas fa-lock" />
                   <input type="password" placeholder="Password" />
                 </div>
+                <div className="danger">{this.state.errors.password}</div>
                 <input type="submit" value="Submit" defaultValue="Login" className="btn solid" />
                 <p className="social-text">Sign in with other platforms</p>
                 <div className="social-media">
@@ -57,7 +147,7 @@ class SignInPage extends Component {
                   </a>
                 </div>
               </form>
-              <form className="sign-up-form">
+              <form className="sign-up-form" onSubmit={this.handleSubmit}>
                 <h2 className="title">Sign up</h2>
                 {/* <div className="input-field">
                   <i className="fas fa-user" />
@@ -65,20 +155,26 @@ class SignInPage extends Component {
                 </div> */}
                 <div className="input-field">
                   <i className="fas fa-envelope" />
-                  <input type="text" placeholder="Email" />
+                  <input type="text" name="email" placeholder="Email" 
+                  value={this.state.input.email} onChange={this.handleChange}/>
                 </div>
                 {/* <div className="input-field">
                   <i className="fas fa-lock" />
                   <input type="phoneNumber" placeholder="Phone Number" />
                 </div> */}
+                <div className="danger">{this.state.errors.password}</div>
                 <div className="input-field">
                   <i className="fas fa-lock" />
-                  <input type="password" placeholder="Password" />
+                  <input type="password" name="password" placeholder="Password" 
+                  value={this.state.input.password} onChange={this.handleChange} />
                 </div>
+                <div className="danger">{this.state.errors.confirm_password}</div>
                 <div className="input-field">
                   <i className="fas fa-lock" />
-                  <input type="password" placeholder="Re-Enter Password" />
+                  <input type="password" name="confirm_password" placeholder="Re-Enter Password" 
+                  value={this.state.input.confirm_password} onChange={this.handleChange} />
                 </div>
+                <div className="danger">{this.state.errors.confirm_password}</div>
                 <input type="submit" value="Submit" defaultValue="Sign up" className="btn solid" />
                 <p className="social-text">Sign up with other platforms</p>
                 <div className="social-media">
