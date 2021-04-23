557d458c16742cc1e7ae6e32ef85ca600d28ccd0
Lyndon-John Lemmers <llemmers1030@yahoo.com>
2021-04-23 00:44:55 -0400

    fixed typo

--- frontend/src/components/MainD/index.js
+++ frontend/src/components/MainD/index.js
@@ -71,7 +71,7 @@ function Main(props) {
             <div className="d-row">
               <p className="dash-blurb" >
                 Thank you for creating an account with Jack Mobile Home Park!
-                In the sidebar you will be able to create maintennance requests,
+                In the sidebar you will be able to create maintenance requests,
                 edit your user settings, and contact the owners for all of your
                 housing needs!
             </p>
