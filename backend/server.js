60b1c858009ad1c90643f7b1fb38803bd06e6b0f
Lyndon-John Lemmers <llemmers1030@yahoo.com>
2021-04-22 23:11:56 -0400

    fix slight error

--- backend/server.js
+++ backend/server.js
@@ -55,10 +55,11 @@ mongoose.connect(MONGO, {
 });*/
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.join(__dirname,"../frontend/build/index.html"));
+    //res.sendFile(path.join(__dirname, "client", "build", "index.html"))
   });
 }else {
   app.get("/", (req, res) => {
